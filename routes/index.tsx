9649db4a357632ac35bee5ca45b27375b04bd3c8
Richard <richard@stromer.org>
2022-09-21 18:08:49 +0200

    use webp image version

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,11 +4,15 @@ export default function Home() {
   return (
     <main class="p-4 mx-auto max-w-screen-md bg-white ring-1 ring-gray-100 shadow-md">
       <div class="flex mb-2">
-        <img
-          src="/me.jpg"
-          class="w-32 h-32 rounded-full aspect-square mr-4"
-          alt="profile picture of richard stromer"
-        />
+        <picture class="w-32 h-32 rounded-full aspect-square mr-4">
+          <source srcset="/me.webp" type="image/webp" />
+          <source srcset="/me.jpg" type="image/jpeg" />
+          <img
+            src="/me.jpg"
+            class="w-32 h-32 rounded-full aspect-square mr-4"
+            alt="profile picture of richard stromer"
+          />
+        </picture>
         <div class="flex flex-col justify-center">
           <h1 class="text-4xl mb-1">Richard Stromer</h1>
           <h2 class="text-xl text-gray-600 mb-2">
