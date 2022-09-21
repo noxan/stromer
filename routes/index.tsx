51da6be4d7c5c81fbca8027a2d317370bc60040b
Richard <richard@stromer.org>
2022-09-21 18:00:52 +0200

    further tweak layout, add shadow

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,16 +2,20 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <main class="p-4 mx-auto max-w-screen-md bg-white ring-1 ring-gray-100">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <h1 class="text-4xl mb-1">Richard Stromer</h1>
-      <h2 class="text-xl text-gray-600 mb-2">
-        Entrepreneur and digital product manager.
-      </h2>
+    <main class="p-4 mx-auto max-w-screen-md bg-white ring-1 ring-gray-100 shadow-md">
+      <div class="flex mb-2">
+        <img
+          src="/me.jpg"
+          class="w-32 h-32 rounded-full aspect-square mr-4"
+          alt="profile picture of richard stromer"
+        />
+        <div class="flex flex-col justify-center">
+          <h1 class="text-4xl mb-1">Richard Stromer</h1>
+          <h2 class="text-xl text-gray-600 mb-2">
+            Entrepreneur and digital product manager.
+          </h2>
+        </div>
+      </div>
       <p class="text-lg mb-6">
         Enthusiastic about building excellent digital products 🧑‍💻=🚀
       </p>
