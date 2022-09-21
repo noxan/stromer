71fd96b8c8cf5b72b01a47523f38bcc88ab2dd21
Richard <richard@stromer.org>
2022-09-21 21:51:01 +0200

    adjust spacing

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,14 +2,14 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <main class="p-4 mx-auto max-w-screen-md bg-white dark:(bg-gray-800 ring-gray-700) ring-1 ring-gray-100 shadow-md rounded-lg">
-      <div class="flex mb-2">
-        <picture class="w-32 h-32 rounded-full aspect-square mr-4">
+    <main class="p-6 mx-auto max-w-screen-md bg-white dark:(bg-gray-800 ring-gray-700) ring-1 ring-gray-100 shadow-md rounded-lg">
+      <div class="flex mb-3">
+        <picture class="w-32 h-32 rounded-full aspect-square mr-5">
           <source srcset="/me.webp" type="image/webp" />
           <source srcset="/me.jpg" type="image/jpeg" />
           <img
             src="/me.jpg"
-            class="w-32 h-32 rounded-full aspect-square mr-4"
+            class="w-32 h-32 rounded-full aspect-square mr-5"
             alt="profile picture of richard stromer"
           />
         </picture>
