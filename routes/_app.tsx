ccc576c59d7a77c71a0359ecc444321af7eed195
Richard <richard@stromer.org>
2022-09-21 14:25:55 +0200

    center content vertically

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from "$fresh/src/server/types.ts";
 
 export default function App({ Component }: AppProps) {
   return (
-    <body class="flex h-full flex-col bg-gray-50">
+    <body class="flex justify-center h-screen flex-col bg-gray-50">
       <Component />
     </body>
   );
