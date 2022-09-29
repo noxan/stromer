43e69f1cbdecea8ab44f5a3f0fcc8cd38b468dfd
Richard <richard@stromer.org>
2022-09-29 15:51:27 +0200

    change title and add meta description tag

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,11 @@ export default function App({ Component }: AppProps) {
   return (
     <html>
       <Head>
-        <title>stromer.org</title>
+        <title>Richard Stromer</title>
+        <meta
+          name="description"
+          content="Entrepreneur and digital product manager. Enthusiastic about building excellent digital products 🧑‍💻=🚀"
+        />
       </Head>
       <body class="antialiased flex justify-center h-screen flex-col sm:(bg-gray-50 dark:(bg-gray-900)) dark:(bg-gray-800 text-gray-100)">
         <Component />
