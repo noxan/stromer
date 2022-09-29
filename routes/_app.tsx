dbe9fd8ee892c08bca741be74ffb1e01f677fd51
Richard <richard@stromer.org>
2022-09-29 15:55:01 +0200

    add meta data for open graph and twitter

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,30 @@
 import { AppProps } from "$fresh/src/server/types.ts";
 import { Head } from "$fresh/runtime.ts";
 
+const title = "Richard Stromer";
+const description =
+  "Entrepreneur and digital product manager. Enthusiastic about building excellent digital products 🧑‍💻=🚀";
+const domain = "stromer.org";
+const website = "https://stromer.org/";
+const image = "https://stromer.org/me.webp";
+
 export default function App({ Component }: AppProps) {
   return (
     <html>
       <Head>
         <title>Richard Stromer</title>
-        <meta
-          name="description"
-          content="Entrepreneur and digital product manager. Enthusiastic about building excellent digital products 🧑‍💻=🚀"
-        />
+        <meta name="description" content={description} />
+        <meta property="og:url" content={website} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content={domain} />
+        <meta property="twitter:url" content={website} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Head>
       <body class="antialiased flex justify-center h-screen flex-col sm:(bg-gray-50 dark:(bg-gray-900)) dark:(bg-gray-800 text-gray-100)">
         <Component />
