f66a424704662ea5350e5e2a723b21b98ab61379
Richard <richard@stromer.org>
2025-05-16 10:52:12 +0200

    migrate existing layout and content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,6 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex h-screen flex-col items-center justify-center">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import IconLinkedIn from '$lib/icons/linkedin.svelte';
+	import IconGithub from '$lib/icons/github.svelte';
+	import IconTwitter from '$lib/icons/twitter.svelte';
+	import IconMedium from '$lib/icons/medium.svelte';
+
+	const profiles = [
+		{ url: 'https://www.linkedin.com/in/richard-stromer/', icon: IconLinkedIn },
+		{ url: 'https://github.com/noxan', icon: IconGithub },
+		{ url: 'https://twitter.com/richardstromer', icon: IconTwitter },
+		{ url: 'https://medium.com/@richard.stromer', icon: IconMedium }
+	];
+</script>
+
+<main
+	class="mx-auto max-w-screen-md p-6 text-center sm:rounded-lg sm:bg-white sm:text-left sm:shadow-md sm:ring-1 sm:ring-gray-100 sm:dark:bg-gray-800 sm:dark:ring-gray-700"
+>
+	<div class="mb-3 flex flex-col items-center sm:flex-row sm:items-stretch">
+		<picture class="mb-3 aspect-square h-32 w-32 rounded-full sm:mr-5">
+			<source srcset="/me.webp" type="image/webp" />
+			<source srcset="/me.jpg" type="image/jpeg" />
+			<img
+				src="/me.jpg"
+				class="mb-3 aspect-square h-32 w-32 rounded-full sm:mr-5"
+				alt="profile of richard stromer"
+			/>
+		</picture>
+		<div class="flex flex-col justify-center">
+			<h1 class="mb-1 text-4xl">Richard Stromer</h1>
+			<h2 class="mb-2 text-xl text-gray-600 dark:text-gray-400">
+				Entrepreneur and digital product manager.
+			</h2>
+		</div>
+	</div>
+	<p class="mb-6 text-lg">Enthusiastic about building excellent digital products 🧑‍💻=🚀</p>
+	<div class="flex flex-row justify-center sm:justify-start">
+		{#each profiles as profile}
+			<a
+				class="fill-current pr-4 text-gray-600 transition hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-200"
+				href={profile.url}
+			>
+				<profile.icon />
+			</a>
+		{/each}
+	</div>
+</main>
